b8c1e53e71f121850dbe4b52386a09e4cef3cb0c
iohio <30587933+iohio@users.noreply.github.com>
2023-11-17 12:38:38 +0700

    update angular test

---
+++ README.md
@@ -0,0 +1,49 @@
+# Employee CRUD Application
+
+This is a simple Employee CRUD (Create, Read, Update, Delete) application that allows you to manage employee records. The application provides basic functionality to add, view, edit, and delete employee information.
+
+## Table of Contents
+
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+  - [Running the Angular Development Server](#running-the-angular-development-server)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- **Create:** Add new employee records to the system.
+- **Read:** View a list of all employees and their details.
+- **Update:** Modify the information of existing employees.
+- **Delete:** Remove employees from the system.
+
+## Technologies Used
+
+- Frontend:
+  - Angular
+  - HTML
+  - CSS
+  - JavaScript
+
+- Backend:
+  - Your preferred backend language (e.g., Node.js, Python, Ruby, etc.)
+  - Your preferred database (e.g., MySQL, PostgreSQL, MongoDB, etc.)
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm
+- Angular CLI (install using `npm install -g @angular/cli`)
+- Your preferred backend language and database installed and configured
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/employee-crud.git
---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,73 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+import { EmployeeService } from '../services/employee.service';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss'],
+})
+export class EmpAddEditComponent implements OnInit {
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Doctor',
+    'Software Engineer',
+    'Backend Dev',
+    'Intern',
+  ];
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      first_name: ['', [Validators.required]],
+      last_name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      date_of_birth: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      education: ['', [Validators.required]],
+      experience: ['', [Validators.required, Validators.min(0)]],
+      company: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    console.log(this.empForm.value)
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService
+          .updateEmployee(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this._coreService.openSnackBar('Employee detail updated!');
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err);
+            },
+          });
+      } else {
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee added successfully');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
